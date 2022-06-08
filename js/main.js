95c00c81704992849ac9233e3c2fd7f3469799ee
PsinaM <zpupka040@gmail.com>
2022-06-08 13:57:08 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
-function getRandomIntInclusive(from, to, amount) {
+function getRandomIntInclusive(from, to) {
   if (from >= 0 && to >= 0 && from <= to) {
     from = Math.ceil(from);
     to = Math.floor(to);
     return Math.floor(Math.random() * (to - from + 1)) + from;
   }
-  else {
-    return null
-  }
+
+  return null
 }
-console.log(getRandomIntInclusive(0, 23))
+
+getRandomIntInclusive(9.8, 42.3)
+
+
+function getRandomIntInclusive2(from, to, digits) {
+  if (from >= 0 && to >= 0 && from <= to && digits >= 0) {
+    return (Math.random() * (to - from + 1) + from).toFixed(digits);
+  }
+
+  return null
+}
+
+getRandomIntInclusive2(9.8, 42.3, 3)
+
