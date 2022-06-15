f79fc3420a6dcfcc754ceb2093d41c83abd58d7b
PsinaM <zpupka040@gmail.com>
2022-06-15 23:24:35 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,55 @@
+const AUTHOR = {
+  avatar: function () {
+   let listAvatars = [];
+
+    for (let i = 1; i <= 10; i++) {
+      if (i < 10) {
+        i = '0' + i;
+      }
+      let avatars = 'img/avatars/user' + i + '.png';
+      listAvatars.push(avatars);
+    }
+    return listAvatars;
+  }
+}
+;
+
+
+const OFFER = {
+  title: 'Заголовок',
+  address: ('location[lat] ' + 'location[lng]'),
+  price: getRandomIntFloat(),
+  type: ['palace', 'flat', 'house', 'bungalow', 'hotel'],
+  rooms: getRandomInt(),
+  guests: getRandomInt(),
+  checkin: ['12:00', '13:00', '14:00'],
+  checkout: ['12:00', '13:00', '14:00'],
+
+  features: function () {
+    let getFeatures = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+    for (let i = 1; i < getFeatures.length; i++) {
+      getFeatures[i] += getRandomInt(0, 10)
+    }
+    return getFeatures;
+  },
+  description: 'Описание помещения',
+  photos: function () {
+    let getPhotos = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+      'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+      'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+    for (let i = 1; i < getPhotos.length; i++) {
+      getPhotos[i] += getRandomInt();
+    }
+    return getPhotos;
+  }
+};
+
+let location =
+  {
+    lat: getRandomIntFloat(35.65000, 35.70000, 5),
+    lng: getRandomIntFloat(139.70000, 139.80000, 5)
+  };
+
 function getRandomInt(from, to) {
   if (from >= 0 && to >= 0 && from <= to) {
     from = Math.ceil(from);
@@ -8,8 +60,6 @@ function getRandomInt(from, to) {
   return null;
 }
 
-getRandomInt(9.8, 42.3);
-
 
 function getRandomIntFloat(from, to, digits) {
   if (from >= 0 && to >= 0 && from <= to && digits >= 0) {
@@ -19,5 +69,22 @@ function getRandomIntFloat(from, to, digits) {
   return null;
 }
 
-getRandomIntFloat(9.8, 42.3, 3);
+
+const getRandomArrayElement = (elements) => {
+  return elements[getRandomInt(0, elements.length - 1)]
+}
+
+
+const createAd = () => {
+  return {
+    author: AUTHOR.avatar,
+    offer: OFFER.title + OFFER.address.location + OFFER.price + getRandomArrayElement(OFFER.type)
+      + OFFER.rooms + OFFER.guests + getRandomArrayElement(OFFER.checkin)
+      + getRandomArrayElement(OFFER.checkout) + OFFER.features + OFFER.photos(),
+    location: location['lat' + ';' + 'lng']
+  };
+};
+
+const ad = Array.from({length: 10}, createAd);
+
 
