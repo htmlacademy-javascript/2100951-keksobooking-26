384a65b46ea4e762c100b08c28dc82c7fcdea8c7
PsinaM <zpupka040@gmail.com>
2022-06-19 14:56:05 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
+
+getRandomInt = (from, to) => {
+  if (from >= 0 && to >= 0 && from <= to) {
+    from = Math.ceil(from);
+    to = Math.floor(to);
+    return Math.floor(Math.random() * (to - from + 1)) + from;
+  }
+
+  return null;
+}
+
+
+getRandomFloat = (from, to, digits) => {
+  if (from >= 0 && to >= 0 && from <= to && digits >= 0) {
+    return (Math.random() * (to - from + 1) + from).toFixed(digits);
+  }
+  return null;
+}
+
+
+const getRandomArrayElement = (elements) => elements[getRandomInt(0, elements.length - 1)];
+
+
 const MAX_ADS = 10;
 
 const PRICE = {
@@ -59,29 +82,6 @@ const createAd = (id) => {
   };
 
 };
-
-
-getRandomInt = (from, to) => {
-  if (from >= 0 && to >= 0 && from <= to) {
-    from = Math.ceil(from);
-    to = Math.floor(to);
-    return Math.floor(Math.random() * (to - from + 1)) + from;
-  }
-
-  return null;
-}
-
-
-getRandomFloat = (from, to, digits) => {
-  if (from >= 0 && to >= 0 && from <= to && digits >= 0) {
-    return (Math.random() * (to - from + 1) + from).toFixed(digits);
-  }
-  return null;
-}
-
-
-const getRandomArrayElement = (elements) => elements[getRandomInt(0, elements.length - 1)];
-
 
 const createAds = (max) => Array.from({length: max},
   (_, index) => createAd(index + 1)
