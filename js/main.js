0e10984761b4db266d2d3db1b50f0750cdd9575f
PsinaM <zpupka040@gmail.com>
2022-07-06 19:23:31 +0300

    module8-task1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
 import './ad.js';
 import './utils.js';
 import './ad-popup.js';
+import './restrictions.js';
---
+++ js/restrictions.js
@@ -0,0 +1,93 @@
+const form = document.querySelector('.ad-form');
+
+const pristine = new Pristine(form, {
+  classTo: 'ad-form__element',
+  errorTextParent: 'ad-form__element',
+  errorTextTag: 'span',
+  errorTextClass: 'form__error'
+});
+
+const address = form.querySelector('#address');
+const latCenter = 35.70000;
+const lngCenter = 139.42500;
+
+const houseTypeField = form.querySelector('#type');
+const housePriceField = form.querySelector('#price');
+
+const setPriceField = (value) => {
+  housePriceField.placeholder=value;
+  housePriceField.min=value;
+};
+
+const setPriceForHouseType = () => {
+  switch (houseTypeField.value) {
+    case 'flat':
+      setPriceField(1000);
+      break;
+    case 'bungalow':
+      setPriceField(0);
+      break;
+    case 'house':
+      setPriceField(5000);
+      break;
+    case 'palace':
+      setPriceField(10000);
+      break;
+    case 'hotel':
+      setPriceField(3000);
+      break;
+  }
+};
+
+window.addEventListener('load', () => {
+  address.value = `${latCenter.toFixed(5)}, ${lngCenter.toFixed(5)  }`;
+  setPriceForHouseType();
+});
+
+houseTypeField.addEventListener('change', () => {
+  setPriceForHouseType();
+});
+
+const validatePrice = () => parseInt(housePriceField.getAttribute('min'), 10) <= housePriceField.value;
+
+pristine.addValidator(
+  form.querySelector('#price'),
+  validatePrice,
+  'Указанная сумма меньше минимальной'
+);
+
+const roomNumber = form.querySelector('#room_number');
+const guestNumber = form.querySelector('#capacity');
+
+const validateGuestNumber = () => roomNumber.value >= guestNumber.value;
+
+const getGuestsErrorMessage = () => {
+  switch (roomNumber.value) {
+    case '3':
+      return 'для 1 гостя, для 2 гостей или для 3 гостей';
+    case '2':
+      return 'для 1 гостя или для 2 гостей';
+    case '1':
+      return 'для 1 гостя';
+    case '0':
+      return 'не для гостей';
+  }
+};
+
+pristine.addValidator(guestNumber, validateGuestNumber, getGuestsErrorMessage);
+
+const timeIn = form.querySelector('#timein');
+const timeOut = form.querySelector('#timeout');
+
+timeIn.addEventListener('change', () => {
+  timeOut.value = timeIn.value;
+});
+timeOut.addEventListener('change', () => {
+  timeIn.value = timeOut.value;
+});
+
+form.addEventListener('submit', (evt) => {
+  if (!pristine.validate()) {
+    evt.preventDefault();
+  }
+});
